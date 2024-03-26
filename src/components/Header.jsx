e592a7f7e5ea20ae003987cb1ea6ea96d0b71389
phuyalbish <phuyalbish@gmail.com>
2024-03-26 15:34:05 +0545

    nav Bar FIxed and made responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import Logo  from '../images/logo.png';
 import { FaBars } from "react-icons/fa6";
@@ -9,29 +9,52 @@ import { MdOutlineLightMode } from "react-icons/md";
 import {AiOutlineClose} from "react-icons/ai";
 
 const Header = () => {
+  const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800 ? true: false)
+  
+  const closeNavHandler = () => {
+    if(window.innerWidth < 800){
+      setIsNavShowing(false);
+    }else{
+      setIsNavShowing(true);
+    }
+  }
+
+
+   const [isNightMode, setIsNightMode] = useState(false)
+  
+  // const setNightHandler = () => {
+  //   if(window.innerWidth < 800){
+  //     setIsNavShowing(false);
+  //   }else{
+  //     setIsNavShowing(true);
+  //   }
+  // }
+  
   return (
     
     <nav>
       <div className="container navbar_container">
         <div className="logonheader">
             
-        <Link to="/" className="nav_logo">
+        <Link to="/" className="nav_logo" onClick={closeNavHandler}>
             <img src={Logo} alt="Nav Logo"/>
         </Link>
         
             <h3>Webodle Inc.</h3>
         </div>
-        <ul className="navbar_menu"> 
-            <li><Link to="/writer">Writers</Link></li>
-            <li><Link to="/create">Create Post</Link></li>
-            <li><Link to="/logout">Logout</Link></li>
-            <li><Link to="/profile/Bishal">Bishal Phuyal</Link></li>
-            <li ><MdOutlineLightMode class="NightLight" onClick={LightMode}/></li>
-            <li><MdOutlineNightlight class="NightLight" onClick={NightMode}/></li>
-            
-        </ul>
-        <button className="toggleNavBar">
-          <AiOutlineClose/>
+        {isNavShowing && <ul className="navbar_menu"> 
+            <li><Link to="/writer"  onClick={closeNavHandler}>Writers</Link></li>
+            <li><Link to="/create"  onClick={closeNavHandler}>Create Post</Link></li>
+            <li><Link to="/logout"  onClick={closeNavHandler}>Logout</Link></li>
+            <li><Link to="/profile/Bishal"  onClick={closeNavHandler}>Bishal Phuyal</Link></li>
+            <li className="nightlightli" onClick={() => setIsNightMode(!isNightMode)}>
+              {
+                isNightMode ? <MdOutlineLightMode class="NightLight" onClick={LightMode}/> : <MdOutlineNightlight class="NightLight" onClick={NightMode}/>
+              }
+              </li>
+        </ul>}
+        <button className="nav_toggle-btn" onClick = {() => setIsNavShowing(!isNavShowing)}>
+          {isNavShowing ? <AiOutlineClose/> : <FaBars/>}
           </button>
       </div>
     </nav>
